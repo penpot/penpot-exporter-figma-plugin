d341eb3fbea57608fed584d3b5abc631dcff0f0e
Jordi Sala Morales <jordism91@gmail.com>
2024-04-10 10:56:14 +0000

    fix

--- src/ui/lib/penpot.d.ts
+++ src/ui/lib/penpot.d.ts
@@ -1,20 +1,23 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CircleShape } from './types/circle/circleShape';
 import { RectShape } from './types/rect/rectShape';
 import { TextShape } from './types/text/textShape';
 
 export interface PenpotFile {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   asMap(): any;
   export(): void;
   addPage(name: string, options?: object): void;
   closePage(): void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   addArtboard(artboard: any): void;
   closeArtboard(): void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   addGroup(group: any): void;
   closeGroup(): void;
   createRect(rect: RectShape): void;
   createCircle(circle: CircleShape): void;
   createText(options: TextShape): void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   createImage(image: any): void;
 }
 
