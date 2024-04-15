dbd0d7f5bdb6fbd3014cd94a4ef4de01211b5bcd
Jordi Sala Morales <jordism91@gmail.com>
2024-04-15 16:50:58 +0000

    try to rotate

--- plugin-src/transformers/transformRectangleNode.ts
+++ plugin-src/transformers/transformRectangleNode.ts
@@ -16,6 +16,15 @@ export const transformRectangleNode = (
     type: 'rect',
     name: node.name,
     fills: translateFills(node.fills, node.width, node.height),
+    transform: {
+      a: node.relativeTransform[0][0],
+      b: -node.relativeTransform[0][1],
+      c: node.relativeTransform[1][0],
+      d: -node.relativeTransform[1][1],
+      e: 0,
+      f: -0
+    },
+    rotation: node.rotation < 0 ? node.rotation + 360 : node.rotation,
     ...transformDimensionAndPosition(node, baseX, baseY),
     ...transformSceneNode(node),
     ...transformBlend(node)
