544271a08c3b49f7bdb92d5cca9f7f5698eac1b6
Alex Sánchez <sion333@gmail.com>
2024-04-17 17:02:24 +0200

    strokes

--- plugin-src/transformers/partials/transformStrokes.ts
+++ plugin-src/transformers/partials/transformStrokes.ts
@@ -2,23 +2,56 @@ import { translateStrokes } from '@plugin/translators';
 
 import { ShapeAttributes } from '@ui/lib/types/shape/shapeAttributes';
 
+export type NodeStrokes = {
+  strokes: readonly Paint[];
+  strokeWeight: number | typeof figma.mixed;
+  strokeAlign: 'CENTER' | 'INSIDE' | 'OUTSIDE';
+  dashPattern: readonly number[];
+};
+
 const isVectorLike = (node: GeometryMixin | VectorLikeMixin): node is VectorLikeMixin => {
   return 'vectorNetwork' in node;
+};
+
+const isIndividualStrokes = (
+  node: DefaultShapeMixin | IndividualStrokesMixin
+): node is IndividualStrokesMixin => {
+  return 'strokeTopWeight' in node;
 };
 
 const hasFillGeometry = (node: GeometryMixin | (GeometryMixin & VectorLikeMixin)): boolean => {
   return node.fillGeometry.length > 0;
 };
 
+const getNodeStrokes = (
+  node: DefaultShapeMixin | (DefaultShapeMixin & IndividualStrokesMixin)
+): NodeStrokes => {
+  return {
+    strokes: node.strokes,
+    strokeWeight: node.strokeWeight,
+    strokeAlign: node.strokeAlign,
+    dashPattern: node.dashPattern
+  };
+};
+
+const getIndividualStrokes = (node: IndividualStrokesMixin): IndividualStrokesMixin => {
+  return {
+    strokeTopWeight: node.strokeTopWeight,
+    strokeRightWeight: node.strokeRightWeight,
+    strokeBottomWeight: node.strokeBottomWeight,
+    strokeLeftWeight: node.strokeLeftWeight
+  };
+};
+
 export const transformStrokes = (
-  node: GeometryMixin | (GeometryMixin & VectorLikeMixin)
+  node: DefaultShapeMixin | (DefaultShapeMixin & IndividualStrokesMixin)
 ): Partial<ShapeAttributes> => {
   return {
     strokes: translateStrokes(
-      node.strokes,
-      node.strokeWeight,
+      getNodeStrokes(node),
       hasFillGeometry(node),
-      isVectorLike(node) ? node.vectorNetwork : undefined
+      isVectorLike(node) ? node.vectorNetwork : undefined,
+      isIndividualStrokes(node) ? getIndividualStrokes(node) : undefined
     )
   };
 };
