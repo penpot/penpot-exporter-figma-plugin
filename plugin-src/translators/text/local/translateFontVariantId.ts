3d64b37302ca02c9a9075f21502fb533e44c0c9a
Jordi Sala Morales <jordism91@gmail.com>
2024-04-29 16:29:32 +0000

    refactor

--- plugin-src/translators/text/local/translateFontVariantId.ts
+++ plugin-src/translators/text/local/translateFontVariantId.ts
@@ -11,23 +11,20 @@ export const translateFontVariantId = (
     variant =>
       variant.weight === fontWeight.toString() && variant.style === (italic ? 'italic' : 'normal')
   );
-  if (variantWithStyleWeight !== undefined) {
-    return variantWithStyleWeight.id;
-  }
+
+  if (variantWithStyleWeight !== undefined) return variantWithStyleWeight.id;
 
   // check match directly by suffix if exists
   const variant = localFont.variants?.find(
     variant => variant.suffix === fontName.style.toLowerCase().replace(/\s/g, '')
   );
-  if (variant !== undefined) {
-    return variant.id;
-  }
+
+  if (variant !== undefined) return variant.id;
 
   // check match directly by id
   const variantById = localFont.variants?.find(
     variant => variant.id === fontName.style.toLowerCase().replace(/\s/g, '')
   );
-  if (variantById !== undefined) {
-    return variantById.id;
-  }
+
+  if (variantById !== undefined) return variantById.id;
 };
