e066977a8981cd51a944ed3080d315542ef2eed3
Alex Sánchez <sion333@gmail.com>
2024-04-29 16:54:23 +0200

    add todo

--- plugin-src/translators/text/custom/findCustomFont.ts
+++ plugin-src/translators/text/custom/findCustomFont.ts
@@ -6,6 +6,7 @@ export const findCustomFont = (fontName: FontName): PenpotFont | undefined => {
   // always send font name if custom font
   figma.ui.postMessage({ type: 'FONT_NAME', data: fontName.family });
 
+  //@TODO: custom font
   return {
     fontId: slugify(fontName.family.toLowerCase()),
     fontVariantId: fontName.style.toLowerCase().replace(/\s/g, '')
