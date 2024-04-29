39280d4c098863d2065beaf5271f4c0dcaaec920
Jordi Sala Morales <jordism91@gmail.com>
2024-04-29 08:11:50 +0000

    remove comment

--- plugin-src/translators/translateLineHeight.ts
+++ plugin-src/translators/translateLineHeight.ts
@@ -1,7 +1,6 @@
 export const translateLineHeight = (
   segment: Pick<StyledTextSegment, 'lineHeight' | 'fontSize'>
 ): number | undefined => {
-  // Penpot reads lineHeight as a multiplier of the font size
   switch (segment.lineHeight.unit) {
     case 'PIXELS':
       return segment.lineHeight.value / segment.fontSize;
