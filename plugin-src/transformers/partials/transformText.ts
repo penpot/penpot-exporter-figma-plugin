6390673d2c46e5f0dbc3a0636462ff84d2c69bdb
Alex Sánchez <sion333@gmail.com>
2024-05-03 12:13:19 +0200

    wip

--- plugin-src/transformers/partials/transformText.ts
+++ plugin-src/transformers/partials/transformText.ts
@@ -14,8 +14,23 @@ import {
   translateVerticalAlign
 } from '@plugin/translators/text';
 
-import { TextStyle } from '@ui/lib/types/text/textContent';
+import { TextNode as PenpotTextNode, TextStyle } from '@ui/lib/types/text/textContent';
 import { TextShape } from '@ui/lib/types/text/textShape';
+
+type StyleTextSegment = Pick<
+  StyledTextSegment,
+  | 'characters'
+  | 'start'
+  | 'end'
+  | 'fontName'
+  | 'fontSize'
+  | 'fontWeight'
+  | 'lineHeight'
+  | 'letterSpacing'
+  | 'textCase'
+  | 'textDecoration'
+  | 'fills'
+>;
 
 export const transformText = (node: TextNode): Partial<TextShape> => {
   const styledTextSegments = node.getStyledTextSegments([
@@ -39,22 +54,7 @@ export const transformText = (node: TextNode): Partial<TextShape> => {
           children: [
             {
               type: 'paragraph',
-              children: styledTextSegments.map((segment, index) => ({
-                fills: translateFills(segment.fills, node.width, node.height),
-                text: translateParagraphSpacing(
-                  node,
-                  translateParagraphIndent(
-                    node,
-                    segment.characters,
-                    segment.fontSize,
-                    translateLetterSpacing(segment),
-                    index
-                  ),
-                  segment.fontSize,
-                  translateLineHeight(segment) ?? 1.2 // Approximation for line height
-                ),
-                ...transformTextStyle(node, segment)
-              })),
+              children: transformStyleTextSegments(node, styledTextSegments),
               ...(styledTextSegments.length ? transformTextStyle(node, styledTextSegments[0]) : {}),
               ...transformFills(node)
             }
@@ -66,23 +66,72 @@ export const transformText = (node: TextNode): Partial<TextShape> => {
   };
 };
 
-const transformTextStyle = (
+const transformStyleTextSegments = (
   node: TextNode,
-  segment: Pick<
-    StyledTextSegment,
-    | 'characters'
-    | 'start'
-    | 'end'
-    | 'fontName'
-    | 'fontSize'
-    | 'fontWeight'
-    | 'lineHeight'
-    | 'letterSpacing'
-    | 'textCase'
-    | 'textDecoration'
-    | 'fills'
-  >
-): Partial<TextStyle> => {
+  segments: StyleTextSegment[]
+): PenpotTextNode[] => {
+  console.log(segments);
+  const textNodes = segments.map(segment => {
+    return transformStyleTextSegment(node, segment);
+  });
+
+  if (node.paragraphIndent === 0) return textNodes;
+
+  const finalTextNodes: PenpotTextNode[] = [];
+  textNodes.forEach(textNode => {
+    const splitTextNode = splitTextNodeByEOL(textNode, node.paragraphIndent);
+    finalTextNodes.push(...splitTextNode);
+  });
+  return finalTextNodes;
+};
+
+const splitTextNodeByEOL = (node: PenpotTextNode, indent: number): PenpotTextNode[] => {
+  const splittedTextNodes: PenpotTextNode[] = [];
+  const foo = node.text.split('\n');
+  if (foo.length === 1) return [node];
+
+  foo.forEach((text, index) => {
+    splittedTextNodes.push(segmentIndent(indent));
+    splittedTextNodes.push({
+      ...node,
+      text: text + (index === foo.length - 1 ? '' : '\n')
+    });
+  });
+
+  return splittedTextNodes;
+};
+
+const needsSegmentIndent = (node: TextNode, segment: StyleTextSegment, index: number): boolean => {
+  if (node.paragraphIndent === 0) return false;
+
+  if (segment.characters.indexOf('\n') === -1) return false;
+
+  return true;
+};
+
+const transformStyleTextSegment = (node: TextNode, segment: StyleTextSegment): PenpotTextNode => {
+  return {
+    fills: translateFills(segment.fills, node.width, node.height),
+    text: segment.characters,
+    ...transformTextStyle(node, segment)
+  };
+};
+
+const segmentIndent = (indent: number): PenpotTextNode => {
+  // space is 10px
+  return {
+    text: ' '.repeat(indent),
+    fontId: 'sourcesanspro',
+    fontVariantId: 'regular',
+    fontSize: '4',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 1,
+    letterSpacing: 0
+  };
+};
+
+const transformTextStyle = (node: TextNode, segment: StyleTextSegment): Partial<TextStyle> => {
   return {
     ...translateFontId(segment.fontName, segment.fontWeight),
     fontFamily: segment.fontName.family,
