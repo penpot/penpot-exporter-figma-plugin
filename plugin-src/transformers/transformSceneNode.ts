aa0346fe25075a9388a1ff54239629e11e710aa9
Jordi Sala Morales <jordism91@gmail.com>
2024-05-09 13:54:31 +0000

    remove old code

--- plugin-src/transformers/transformSceneNode.ts
+++ plugin-src/transformers/transformSceneNode.ts
@@ -14,19 +14,6 @@ export const transformSceneNode = async (
   baseX: number = 0,
   baseY: number = 0
 ): Promise<PenpotNode | undefined> => {
-  // @TODO: when penpot 2.0, manage image as fills for the basic types
-  // if (
-  //   'fills' in node &&
-  //   node.fills !== figma.mixed &&
-  //   node.fills.find(fill => fill.type === 'IMAGE')
-  // ) {
-  //   // If the nested frames extended the bounds of the rasterized image, we need to
-  //   // account for this both in position on the canvas and the calculated width and
-  //   // height of the image.
-  //   const [adjustedX, adjustedY] = calculateAdjustment(node);
-  //   return await transformImageNode(node, baseX + adjustedX, baseY + adjustedY);
-  // }
-
   switch (node.type) {
     case 'RECTANGLE':
       return await transformRectangleNode(node, baseX, baseY);
