006bf97a6a47af91a1f55793fdfc33c21bebdef5
Jordi Sala Morales <jordism91@gmail.com>
2024-05-09 16:23:29 +0000

    Performance improvements

--- plugin-src/transformers/transformDocumentNode.ts
+++ plugin-src/transformers/transformDocumentNode.ts
@@ -1,10 +1,19 @@
 import { PenpotDocument } from '@ui/lib/types/penpotDocument';
+import { PenpotPage } from '@ui/lib/types/penpotPage';
 
 import { transformPageNode } from '.';
 
 export const transformDocumentNode = async (node: DocumentNode): Promise<PenpotDocument> => {
+  const children: PenpotPage[] = [];
+
+  for (const child of node.children) {
+    await child.loadAsync();
+
+    children.push(await transformPageNode(child));
+  }
+
   return {
     name: node.name,
-    children: await Promise.all(node.children.map(child => transformPageNode(child)))
+    children
   };
 };
