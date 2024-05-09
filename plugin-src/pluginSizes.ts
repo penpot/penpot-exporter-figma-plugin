04fe233c15d4070745c8b8a685cc292644541dcd
Jordi Sala Morales <jordism91@gmail.com>
2024-05-09 10:14:17 +0000

    adjust sizes

--- plugin-src/pluginSizes.ts
+++ plugin-src/pluginSizes.ts
@@ -1,8 +1,8 @@
-export const LOADING_HEIGHT = 150;
-export const NORMAL_HEIGHT = LOADING_HEIGHT + 10;
+export const LOADING_HEIGHT = 130;
+export const NORMAL_HEIGHT = LOADING_HEIGHT + 5;
 export const BASE_WIDTH = 290;
 
-export const MISSING_FONTS_TEXT_HEIGHT = 205;
+export const MISSING_FONTS_TEXT_HEIGHT = 210;
 export const MISSING_SINGLE_FONT_HEIGHT = 70;
 
-export const NEEDS_RELOAD_TEXT_HEIGHT = 96;
+export const NEEDS_RELOAD_TEXT_HEIGHT = 100;
