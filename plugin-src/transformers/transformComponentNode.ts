5917da8ddad0e2c9e551003bbe3d744370501b1c
Jordi Sala Morales <jordism91@gmail.com>
2024-05-13 11:44:02 +0000

    revert

--- plugin-src/transformers/transformComponentNode.ts
+++ plugin-src/transformers/transformComponentNode.ts
@@ -1,25 +1,18 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { transformDimensionAndPosition } from '@plugin/transformers/partials';
 import { transformChildren } from '@plugin/transformers/partials';
 
-import { ComponentShape } from '@ui/lib/types/component/componentShape';
+import { ComponentShape } from '@ui/lib/types/shapes/componentShape';
 
 export const transformComponentNode = async (
   node: ComponentNode,
   baseX: number,
   baseY: number
 ): Promise<ComponentShape> => {
-  console.log(self);
-
-  const id = uuidv4();
-
   return {
-    id,
     type: 'component',
     name: node.name,
     path: '',
-    mainInstanceId: id,
+    mainInstanceId: '',
     mainInstancePage: '',
     ...(await transformChildren(node, baseX + node.x, baseY + node.y)),
     ...transformDimensionAndPosition(node, baseX, baseY)
--- ui-src/converters/createPenpotComponent.ts
+++ ui-src/converters/createPenpotComponent.ts
@@ -1,7 +1,7 @@
-
-import { ComponentShape } from '@ui/lib/types/shapes/componentShape';
-import { createPenpotItem } from '.';
 import { PenpotFile } from '@ui/lib/types/penpotFile';
+import { ComponentShape } from '@ui/lib/types/shapes/componentShape';
+
+import { createPenpotItem } from '.';
 
 export const createPenpotComponent = (
   file: PenpotFile,
