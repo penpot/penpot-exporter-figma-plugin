de12d0a15e782b5d35c55a3459459224b346406d
Jordi Sala Morales <jordism91@gmail.com>
2024-05-14 14:42:26 +0000

    fix

--- plugin-src/transformers/partials/transformVectorPaths.ts
+++ plugin-src/transformers/partials/transformVectorPaths.ts
@@ -193,7 +193,7 @@ const translateVectorSegmentToSvgPath = (
   end: VectorVertex,
   isFirst: boolean
 ): string => {
-  const path = isFirst ? `M ${start.x} ${start.y}` : '';
+  const path = isFirst ? `M ${start.x} ${start.y} ` : '';
 
   const c1 = segment.tangentStart;
   const c2 = segment.tangentEnd;
@@ -201,7 +201,7 @@ const translateVectorSegmentToSvgPath = (
   if (c1 && c2 && c1.x > 0) {
     return (
       path +
-      ` C ${start.x + c1.x} ${start.y + c1.y}, ${end.x + c2.x} ${end.y + c2.y}, ${end.x} ${end.y}`
+      `C ${start.x + c1.x} ${start.y + c1.y}, ${end.x + c2.x} ${end.y + c2.y}, ${end.x} ${end.y}`
     );
   }
 
