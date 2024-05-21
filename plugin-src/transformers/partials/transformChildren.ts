116280f09d028ad1f8c1ef5c2dab6b8ddb1ecbb5
Alex Sánchez <sion333@gmail.com>
2024-05-21 08:51:50 +0200

    refactor

--- plugin-src/transformers/partials/transformChildren.ts
+++ plugin-src/transformers/partials/transformChildren.ts
@@ -1,16 +1,53 @@
-import { transformSceneNode } from '@plugin/transformers';
+import { transformGroupNodeLike, transformSceneNode } from '@plugin/transformers';
 
 import { PenpotNode } from '@ui/lib/types/penpotNode';
+import { GroupShape } from '@ui/lib/types/shapes/groupShape';
 import { Children } from '@ui/lib/types/utils/children';
 
 export const transformChildren = async (
-  node: { children: SceneNode[] },
+  node: ChildrenMixin,
   baseX: number = 0,
   baseY: number = 0
 ): Promise<Children> => {
   return {
-    children: (
-      await Promise.all(node.children.map(child => transformSceneNode(child, baseX, baseY)))
-    ).filter((child): child is PenpotNode => !!child)
+    children: await splitChildrenWithMask(node.children, baseX, baseY)
   };
+};
+
+const splitChildrenWithMask = async (
+  children: readonly SceneNode[],
+  baseX: number,
+  baseY: number
+): Promise<PenpotNode[]> => {
+  const splitChildren: PenpotNode[] = [];
+  let currentGroup: Partial<GroupShape> | null = null;
+
+  for (const child of children) {
+    const transformedChild = await transformSceneNode(child, baseX, baseY);
+
+    if ('isMask' in child && child.isMask) {
+      if (currentGroup?.children?.length) {
+        splitChildren.push(currentGroup as GroupShape);
+      }
+      currentGroup = {
+        ...transformGroupNodeLike(child, baseX, baseY),
+        children: [],
+        maskedGroup: true
+      };
+    }
+
+    if (transformedChild) {
+      if (currentGroup) {
+        currentGroup.children?.push(transformedChild);
+      } else {
+        splitChildren.push(transformedChild);
+      }
+    }
+  }
+
+  if (currentGroup?.children?.length) {
+    splitChildren.push(currentGroup as GroupShape);
+  }
+
+  return splitChildren;
 };
