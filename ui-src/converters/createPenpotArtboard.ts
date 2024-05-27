6c078bafb9717b2b939b59ae045d9ec04afb6a06
Jordi Sala Morales <jordism91@gmail.com>
2024-05-27 09:55:34 +0000

    not needed, refactor

--- ui-src/converters/createPenpotArtboard.ts
+++ ui-src/converters/createPenpotArtboard.ts
@@ -1,21 +1,18 @@
 import { PenpotFile } from '@ui/lib/types/penpotFile';
 import { ComponentShape } from '@ui/lib/types/shapes/componentShape';
 import { FrameShape } from '@ui/lib/types/shapes/frameShape';
-import { Uuid } from '@ui/lib/types/utils/uuid';
 
 import { createPenpotItem } from '.';
 
 export const createPenpotArtboard = (
   file: PenpotFile,
   { type, children = [], ...rest }: FrameShape | ComponentShape
-): Uuid => {
-  const id = file.addArtboard(rest);
+) => {
+  file.addArtboard(rest);
 
   for (const child of children) {
     createPenpotItem(file, child);
   }
 
   file.closeArtboard();
-
-  return id;
 };
