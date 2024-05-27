761ab1f002229802ea7096ac3f15ed550c0cd6aa
Jordi Sala Morales <jordism91@gmail.com>
2024-05-27 09:50:01 +0000

    fix name

--- plugin-src/translators/translateChildren.ts
+++ plugin-src/translators/translateChildren.ts
@@ -9,17 +9,17 @@ import { PenpotNode } from '@ui/lib/types/penpotNode';
  * The masked children will be grouped together in a mask group.
  * The unmasked children will be returned as they are.
  *
- * @maskedIndex The index of the mask node in the children array
+ * @maskIndex The index of the mask node in the children array
  */
 export const translateMaskChildren = async (
   children: readonly SceneNode[],
-  maskedIndex: number,
+  maskIndex: number,
   baseX: number,
   baseY: number
 ): Promise<PenpotNode[]> => {
-  const maskChild = children[maskedIndex];
-  const unmaskedChildren = await translateChildren(children.slice(0, maskedIndex), baseX, baseY);
-  const maskedChildren = await translateChildren(children.slice(maskedIndex), baseX, baseY);
+  const maskChild = children[maskIndex];
+  const unmaskedChildren = await translateChildren(children.slice(0, maskIndex), baseX, baseY);
+  const maskedChildren = await translateChildren(children.slice(maskIndex), baseX, baseY);
 
   const maskGroup = {
     ...transformGroupNodeLike(maskChild, baseX, baseY),
