3f9189c7c1accf2b738b2901fdd1ef85a1765ecc
Alex Sánchez <sion333@gmail.com>
2024-06-03 10:06:18 +0200

    Filtering design system nodes (#130)
    * filtering design system nodes
    
    * Delete .changeset/eight-berries-hunt.md

--- plugin-src/transformers/transformInstanceNode.ts
+++ plugin-src/transformers/transformInstanceNode.ts
@@ -24,9 +24,10 @@ export const transformInstanceNode = async (
    * We do not want to process component instances in the following scenarios:
    *
    * 1. If the component does not have a main component.
-   * 2. If the component does not have parent (it comes from an external design system).
+   * 2. If the component comes from an external design system.
+   * 3. If th component does not have a parent. (it's been removed)
    */
-  if (!mainComponent || mainComponent.parent === null) {
+  if (!mainComponent || mainComponent.remote || mainComponent.parent === null) {
     return;
   }
 
