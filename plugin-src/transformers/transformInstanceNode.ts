90d6dfd95a24be88918b55fb3278ac1c2f6d00d8
Alex Sánchez <sion333@gmail.com>
2024-06-03 13:27:30 +0200

    Filter missing component set (#131)
    * filtering missing component sets
    
    * fixes
    
    * fixes
    
    * fixes

--- plugin-src/transformers/transformInstanceNode.ts
+++ plugin-src/transformers/transformInstanceNode.ts
@@ -20,21 +20,14 @@ export const transformInstanceNode = async (
 ): Promise<ComponentInstance | undefined> => {
   const mainComponent = await node.getMainComponentAsync();
 
-  /**
-   * We do not want to process component instances in the following scenarios:
-   *
-   * 1. If the component does not have a main component.
-   * 2. If the component comes from an external design system.
-   * 3. If th component does not have a parent. (it's been removed)
-   */
-  if (!mainComponent || mainComponent.remote || mainComponent.parent === null) {
+  if (!isNodeProcessable(node, mainComponent)) {
     return;
   }
 
   return {
     type: 'instance',
     name: node.name,
-    mainComponentFigmaId: mainComponent.id,
+    mainComponentFigmaId: mainComponent?.id ?? '',
     isComponentRoot: isComponentRoot(node),
     ...transformFigmaIds(node),
     ...(await transformFills(node)),
@@ -47,6 +40,24 @@ export const transformInstanceNode = async (
     ...transformDimensionAndPosition(node, baseX, baseY),
     ...(await transformChildren(node, baseX + node.x, baseY + node.y))
   };
+};
+
+const isNodeProcessable = (node: SceneNode, mainComponent: ComponentNode | null): boolean => {
+  /**
+   * We do not want to process component instances in the following scenarios:
+   *
+   * 1. If the component does not have a main component.
+   * 2. If the component comes from an external design system.
+   * 3. If th component does not have a parent. (it's been removed)
+   * 4. Main component can be in a ComponentSet removed from the page or external design system.
+   */
+  return !(
+    !mainComponent ||
+    mainComponent.remote ||
+    mainComponent.parent === null ||
+    (mainComponent.parent?.type === 'COMPONENT_SET' &&
+      (mainComponent.parent.parent === null || mainComponent.parent.remote))
+  );
 };
 
 const isComponentRoot = (node: InstanceNode): boolean => {
