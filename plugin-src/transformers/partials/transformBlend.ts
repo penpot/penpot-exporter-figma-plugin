d9b2fcca0b7d061cb972b883d0495e8117802753
Jordi Sala Morales <jordism91@gmail.com>
2024-06-07 08:49:04 +0000

    Respect nodes visibility and opacity

--- plugin-src/transformers/partials/transformBlend.ts
+++ plugin-src/transformers/partials/transformBlend.ts
@@ -4,9 +4,10 @@ import { ShapeAttributes } from '@ui/lib/types/shapes/shape';
 
 export const transformBlend = (
   node: SceneNodeMixin & MinimalBlendMixin
-): Pick<ShapeAttributes, 'blendMode' | 'opacity'> => {
+): Pick<ShapeAttributes, 'blendMode' | 'hidden' | 'opacity'> => {
   return {
     blendMode: translateBlendMode(node.blendMode),
-    opacity: !node.visible ? 0 : node.opacity // @TODO: check this. If we use the property hidden and it's hidden, it won't export
+    hidden: !node.visible,
+    opacity: node.opacity
   };
 };
