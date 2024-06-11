c44fa0cbdd1f7bf7ef9dd3f4c50ebf5225788fab
Alex Sánchez <sion333@gmail.com>
2024-06-11 09:56:07 +0200

    fixes

--- plugin-src/transformers/partials/transformBlend.ts
+++ plugin-src/transformers/partials/transformBlend.ts
@@ -4,10 +4,9 @@ import { ShapeAttributes } from '@ui/lib/types/shapes/shape';
 
 export const transformBlend = (
   node: SceneNodeMixin & MinimalBlendMixin
-): Pick<ShapeAttributes, 'blendMode' | 'hidden' | 'opacity'> => {
+): Pick<ShapeAttributes, 'blendMode' | 'opacity'> => {
   return {
     blendMode: translateBlendMode(node.blendMode),
-    hidden: !node.visible,
     opacity: node.opacity
   };
 };
