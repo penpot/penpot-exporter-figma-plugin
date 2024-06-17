202e7f4fda62980094e447a7763864ab2c48624d
Jordi Sala Morales <jordism91@gmail.com>
2024-06-17 12:14:16 +0200

    Apply rotations to any figure (#168)
    * Apply rotations to any figure
    
    * add changelog
    
    * apply rotations to curves too

--- plugin-src/utils/applyRotation.ts
+++ plugin-src/utils/applyRotation.ts
@@ -1,3 +1,4 @@
+import { ClosePath, CurveTo, Segment } from '@ui/lib/types/shapes/pathShape';
 import { Point } from '@ui/lib/types/utils/point';
 
 const ROTATION_TOLERANCE = 0.000001;
@@ -14,6 +15,41 @@ export const applyRotation = (point: Point, transform: Transform, boundingBox: R
     x: centerPoint.x + rotatedPoint.x,
     y: centerPoint.y + rotatedPoint.y
   };
+};
+
+export const applyRotationToSegment = (
+  segment: Exclude<Segment, ClosePath>,
+  transform: Transform,
+  boundingBox: Rect
+): Segment => {
+  const rotated = applyRotation(
+    { x: segment.params.x, y: segment.params.y },
+    transform,
+    boundingBox
+  );
+
+  if (isCurveTo(segment)) {
+    const curve1 = applyRotation(
+      { x: segment.params.c1x, y: segment.params.c1y },
+      transform,
+      boundingBox
+    );
+    const curve2 = applyRotation(
+      { x: segment.params.c2x, y: segment.params.c2y },
+      transform,
+      boundingBox
+    );
+
+    segment.params.c1x = curve1.x;
+    segment.params.c1y = curve1.y;
+    segment.params.c2x = curve2.x;
+    segment.params.c2y = curve2.y;
+  }
+
+  segment.params.x = rotated.x;
+  segment.params.y = rotated.y;
+
+  return segment;
 };
 
 export const applyInverseRotation = (
@@ -38,3 +74,5 @@ const calculateCenter = (boundingBox: Rect): Point => ({
   x: boundingBox.x + boundingBox.width / 2,
   y: boundingBox.y + boundingBox.height / 2
 });
+
+const isCurveTo = (segment: Segment): segment is CurveTo => segment.command === 'curve-to';
