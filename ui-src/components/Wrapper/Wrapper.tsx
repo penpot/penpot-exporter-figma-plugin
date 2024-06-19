bc33c0ed9c3000d65ec1a18ec09e052b19bbb356
Jordi Sala Morales <jordism91@gmail.com>
2024-06-19 17:01:37 +0200

    Fix plugin scrollbar (#181)

--- ui-src/components/Wrapper/Wrapper.tsx
+++ ui-src/components/Wrapper/Wrapper.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import { CSSProperties, PropsWithChildren, forwardRef } from 'react';
 
 import styles from './Wrapper.module.css';
@@ -11,11 +10,7 @@ type WrapperProps = PropsWithChildren & {
 const Wrapper = forwardRef<HTMLDivElement, WrapperProps>(
   ({ style, overflowing = false, children }: WrapperProps, ref) => {
     return (
-      <div
-        ref={ref}
-        className={classNames({ [styles.wrapper]: true, [styles.wrapperOverflow]: overflowing })}
-        style={style}
-      >
+      <div ref={ref} className={styles.wrapper} style={style} data-overflowing={overflowing}>
         {children}
       </div>
     );
