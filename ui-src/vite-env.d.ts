586a5f861fc63e0f16d8b711fdfd9c254b78eba2
Alex Sánchez <alejandro@runroom.com>
2024-10-17 16:01:12 +0200

    metricts-sentry

--- ui-src/vite-env.d.ts
+++ ui-src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+declare module ViteEnv {
+  interface ImportMetaEnv {
+    VITE_SENTRY_DSN: string;
+  }
+}
+
+declare const APP_VERSION: string;
