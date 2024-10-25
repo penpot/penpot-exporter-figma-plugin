29db77aa7a915fe47d35cd3e268d4b3c88e90080
Alex Sánchez <alejandro@runroom.com>
2024-10-25 08:28:51 +0200

    refactor

--- ui-src/main.tsx
+++ ui-src/main.tsx
@@ -1,24 +1,14 @@
-import * as Sentry from '@sentry/react';
 import 'node_modules/@create-figma-plugin/ui/lib/css/base.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import { initializeSentry } from '@ui/metrics/sentry';
 
 import { App } from './App';
 import './main.css';
 import './reset.css';
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  release: `penpot-exporter@${APP_VERSION}`,
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+initializeSentry();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
