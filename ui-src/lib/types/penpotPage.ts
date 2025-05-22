78919d75f5b0426658491ee00f680ed894cd44a4
Alex Sánchez <alejandro@runroom.com>
2025-05-22 13:05:03 +0200

    Remove unused data

--- ui-src/lib/types/penpotPage.ts
+++ ui-src/lib/types/penpotPage.ts
@@ -1,5 +1,4 @@
 import { Children } from '@ui/lib/types/utils/children';
-import { SavedGrids } from '@ui/lib/types/utils/grid';
 import { Uuid } from '@ui/lib/types/utils/uuid';
 
 export type PenpotPage = {
@@ -7,23 +6,3 @@ export type PenpotPage = {
   name: string;
   background?: string;
 } & Children;
-
-export type PenpotPageOptions = {
-  background?: string;
-  savedGrids?: SavedGrids;
-  flows?: Flow[];
-  guides?: { [uuid: Uuid]: Guide };
-};
-
-type Flow = {
-  id?: Uuid;
-  name: string;
-  startingFrame: Uuid;
-};
-
-type Guide = {
-  id?: Uuid;
-  axis: 'x' | 'y';
-  position: number;
-  frameId?: Uuid;
-};
