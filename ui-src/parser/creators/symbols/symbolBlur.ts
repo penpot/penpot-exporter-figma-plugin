79f6fffdde7f44099ff98cca3a0a11abb20d3468
Alex Sánchez <alejandro@runroom.com>
2025-05-23 10:32:35 +0200

    Fix blur

--- ui-src/parser/creators/symbols/symbolBlur.ts
+++ ui-src/parser/creators/symbols/symbolBlur.ts
@@ -1,10 +1,11 @@
 import { PenpotContext } from '@ui/lib/types/penpotContext';
 import { Blur } from '@ui/lib/types/utils/blur';
+import { parseFigmaId } from '@ui/parser/parseFigmaId';
 
 export const symbolBlur = (context: PenpotContext, blur: Blur | undefined): Blur | undefined => {
   if (!blur) return;
 
-  const blurId = context.genId();
+  const blurId = parseFigmaId(context);
 
   return {
     ...blur,
