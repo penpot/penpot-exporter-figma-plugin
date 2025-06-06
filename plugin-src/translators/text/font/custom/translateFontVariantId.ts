698b0d4c0c86e20c5a50297e85987e69949c0cd2
Jordi <jordism91@gmail.com>
2025-06-06 10:48:38 +0200

    Fallback font variant id

--- plugin-src/translators/text/font/custom/translateFontVariantId.ts
+++ plugin-src/translators/text/font/custom/translateFontVariantId.ts
@@ -1,4 +1,7 @@
-export const translateFontVariantId = (fontName: FontName | undefined, fontWeight: string) => {
+export const translateFontVariantId = (
+  fontName: FontName | undefined,
+  fontWeight: string
+): string => {
   const style = fontName?.style.toLowerCase().includes('italic') ? 'italic' : 'normal';
 
   return `${style}-${fontWeight}`;
