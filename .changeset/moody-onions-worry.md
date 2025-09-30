e8743e3c2f620b90c37075419b4c3c06b08a30ea
Jordi Sala Morales <jordism91@gmail.com>
2025-09-30 11:08:00 +0200

    Disable sentry while developing the plugin (#273)
    * Disable sentry while developing the plugin
    
    * add changelog

---
+++ .changeset/moody-onions-worry.md
@@ -0,0 +1,5 @@
+---
+'penpot-exporter': patch
+---
+
+Disable Sentry in development mode
--- vite.config.ts
+++ vite.config.ts
@@ -6,37 +6,40 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  root: './ui-src',
-  plugins: [
-    svgr(),
-    react(),
-    viteSingleFile({ removeViteModuleLoader: true }),
-    tsconfigPaths(),
-    sentryVitePlugin({
-      org: 'runroom-sl',
-      project: 'penpot-exporter'
-    })
-  ],
-  resolve: {
-    alias: {
-      'react': 'preact/compat',
-      'react-dom': 'preact/compat',
-      '!../css/base.css': '../css/base.css'
-    }
-  },
-  build: {
-    emptyOutDir: false,
-    target: 'esnext',
-    reportCompressedSize: false,
-    outDir: '../dist',
-
-    rollupOptions: {
-      external: ['!../css/base.css']
+export default ({ mode }) => {
+  return defineConfig({
+    root: './ui-src',
+    plugins: [
+      svgr(),
+      react(),
+      viteSingleFile({ removeViteModuleLoader: true }),
+      tsconfigPaths(),
+      sentryVitePlugin({
+        org: 'runroom-sl',
+        project: 'penpot-exporter',
+        disable: mode === 'development'
+      })
+    ],
+    resolve: {
+      alias: {
+        'react': 'preact/compat',
+        'react-dom': 'preact/compat',
+        '!../css/base.css': '../css/base.css'
+      }
     },
-    sourcemap: true
-  },
-  define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version)
-  }
-});
+    build: {
+      emptyOutDir: false,
+      target: 'esnext',
+      reportCompressedSize: false,
+      outDir: '../dist',
+
+      rollupOptions: {
+        external: ['!../css/base.css']
+      },
+      sourcemap: true
+    },
+    define: {
+      APP_VERSION: JSON.stringify(process.env.npm_package_version)
+    }
+  });
+};
