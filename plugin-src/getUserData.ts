de71283184e8bb28dd3d5bed6d629d9f97f5d746
Jordi <jordism91@gmail.com>
2025-11-12 15:37:30 +0100

    Improve

--- plugin-src/getUserData.ts
+++ plugin-src/getUserData.ts
@@ -1,13 +1,16 @@
 export const getUserData = (): void => {
   const user = figma.currentUser;
-  if (user) {
-    figma.ui.postMessage({
-      type: 'USER_DATA',
-      data: {
-        userId: user.id
-      }
-    });
-  } else {
+
+  if (!user) {
     console.warn('Could not get user data');
+
+    return;
   }
+
+  figma.ui.postMessage({
+    type: 'USER_DATA',
+    data: {
+      userId: user.id
+    }
+  });
 };
