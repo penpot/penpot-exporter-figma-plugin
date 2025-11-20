69b4f9a7980ef55c1af5a85995a89705da69040c
Jordi <jordism91@gmail.com>
2025-11-20 11:01:14 +0100

    improve code clarity

--- plugin-src/translators/translateAppliedTokens.ts
+++ plugin-src/translators/translateAppliedTokens.ts
@@ -2,34 +2,34 @@ import { variables } from '@plugin/libraries';
 
 import type { TokenProperties } from '@ui/lib/types/shapes/tokens';
 
-const equivalences = [
+const EQUIVALENCIES = [
   // Color
-  { variable: 'fills', token: 'fill', type: 'color' },
-  { variable: 'strokes', token: 'strokeColor', type: 'color' },
+  { figma: 'fills', penpot: 'fill', tokenType: 'color' },
+  { figma: 'strokes', penpot: 'strokeColor', tokenType: 'color' },
 
   // Number
-  { variable: 'opacity', token: 'opacity', type: 'opacity' },
-  { variable: 'topLeftRadius', token: 'r1', type: 'borderRadius' },
-  { variable: 'topRightRadius', token: 'r2', type: 'borderRadius' },
-  { variable: 'bottomRightRadius', token: 'r3', type: 'borderRadius' },
-  { variable: 'bottomLeftRadius', token: 'r4', type: 'borderRadius' },
-  { variable: 'itemSpacing', token: 'rowGap', type: 'spacing' },
-  { variable: 'itemSpacing', token: 'columnGap', type: 'spacing' },
-  { variable: 'width', token: 'width', type: 'sizing' },
-  { variable: 'height', token: 'height', type: 'sizing' },
-  { variable: 'paddingTop', token: 'p1', type: 'spacing' },
-  { variable: 'paddingRight', token: 'p2', type: 'spacing' },
-  { variable: 'paddingBottom', token: 'p3', type: 'spacing' },
-  { variable: 'paddingLeft', token: 'p4', type: 'spacing' },
-  { variable: 'letterSpacing', token: 'letterSpacing', type: 'letterSpacing' },
-  { variable: 'fontSize', token: 'fontSize', type: 'fontSizes' },
-  { variable: 'strokeTopWeight', token: 'strokeWidth', type: 'borderWidth' },
+  { figma: 'opacity', penpot: 'opacity', tokenType: 'opacity' },
+  { figma: 'topLeftRadius', penpot: 'r1', tokenType: 'borderRadius' },
+  { figma: 'topRightRadius', penpot: 'r2', tokenType: 'borderRadius' },
+  { figma: 'bottomRightRadius', penpot: 'r3', tokenType: 'borderRadius' },
+  { figma: 'bottomLeftRadius', penpot: 'r4', tokenType: 'borderRadius' },
+  { figma: 'itemSpacing', penpot: 'rowGap', tokenType: 'spacing' },
+  { figma: 'itemSpacing', penpot: 'columnGap', tokenType: 'spacing' },
+  { figma: 'width', penpot: 'width', tokenType: 'sizing' },
+  { figma: 'height', penpot: 'height', tokenType: 'sizing' },
+  { figma: 'paddingTop', penpot: 'p1', tokenType: 'spacing' },
+  { figma: 'paddingRight', penpot: 'p2', tokenType: 'spacing' },
+  { figma: 'paddingBottom', penpot: 'p3', tokenType: 'spacing' },
+  { figma: 'paddingLeft', penpot: 'p4', tokenType: 'spacing' },
+  { figma: 'letterSpacing', penpot: 'letterSpacing', tokenType: 'letterSpacing' },
+  { figma: 'fontSize', penpot: 'fontSize', tokenType: 'fontSizes' },
+  { figma: 'strokeTopWeight', penpot: 'strokeWidth', tokenType: 'borderWidth' },
 
   // Text
-  { variable: 'fontFamily', token: 'fontFamily', type: 'fontFamilies' },
+  { figma: 'fontFamily', penpot: 'fontFamily', tokenType: 'fontFamilies' },
 
   // Number & Text
-  { variable: 'fontWeight', token: 'fontWeight', type: 'fontWeights' }
+  { figma: 'fontWeight', penpot: 'fontWeight', tokenType: 'fontWeights' }
 ] as const;
 
 const getBoundVariableIdForPaint = (paints: readonly Paint[]): string | null => {
@@ -121,18 +121,18 @@ export const translateAppliedTokens = (
 
   const appliedTokens: { [key in TokenProperties]?: string } = {};
 
-  for (const equivalence of equivalences) {
-    const boundVariable = boundVariables[equivalence.variable];
+  for (const equivalence of EQUIVALENCIES) {
+    const boundVariable = boundVariables[equivalence.figma];
 
     if (!boundVariable) continue;
 
-    const boundVariableId = getBoundVariableId(boundVariable, equivalence.variable, node);
+    const boundVariableId = getBoundVariableId(boundVariable, equivalence.figma, node);
     if (!boundVariableId) continue;
 
-    const variable = variables.get(`${boundVariableId}.${equivalence.type}`);
+    const variable = variables.get(`${boundVariableId}.${equivalence.tokenType}`);
 
     if (variable) {
-      appliedTokens[equivalence.token] = variable;
+      appliedTokens[equivalence.penpot] = variable;
     }
   }
 
