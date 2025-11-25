95bd5df41607bcb0b338a80e4f8876d2f4de426c
Jordi <jordism91@gmail.com>
2025-11-25 12:27:13 +0100

    improve

--- plugin-src/transformers/transformInstanceNode.ts
+++ plugin-src/transformers/transformInstanceNode.ts
@@ -32,21 +32,19 @@ export const transformInstanceNode = async (
 
   const primaryComponent = getPrimaryComponent(mainComponent);
   const isOrphan = isOrphanInstance(primaryComponent);
-  let nodeOverrides = {};
 
   if (!isOrphan && node.overrides.length > 0) {
     node.overrides.forEach(override => overrides.set(override.id, override.overriddenFields));
-    nodeOverrides = transformOverrides(node);
   }
 
-  // const fetchedOverrides = [...(overrides.get(node.id) ?? [])];
-  // if (node.visible !== mainComponent.visible) {
-  //   fetchedOverrides.push('visible');
-  // }
-  // if (node.locked !== mainComponent.locked) {
-  //   fetchedOverrides.push('locked');
-  // }
-  // overrides.set(node.id, fetchedOverrides);
+  const fetchedOverrides = [...(overrides.get(node.id) ?? [])];
+  if (node.visible !== mainComponent.visible) {
+    fetchedOverrides.push('visible');
+  }
+  if (node.locked !== mainComponent.locked) {
+    fetchedOverrides.push('locked');
+  }
+  overrides.set(node.id, fetchedOverrides);
 
   return {
     type: 'instance',
@@ -71,7 +69,7 @@ export const transformInstanceNode = async (
     ...transformVariableConsumptionMap(node),
     ...transformGrids(node),
     ...(await transformChildren(node)),
-    ...nodeOverrides
+    ...transformOverrides(node)
   };
 };
 
