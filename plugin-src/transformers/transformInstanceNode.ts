5944a223204118865d4f58d69a49af8b9eb8b4ef
Jordi <jordism91@gmail.com>
2026-01-14 12:54:41 +0100

    Apply overrides for remote components that are not removed

--- plugin-src/transformers/transformInstanceNode.ts
+++ plugin-src/transformers/transformInstanceNode.ts
@@ -82,7 +82,7 @@ const getPrimaryComponent = (mainComponent: ComponentNode): ComponentNode | Comp
 };
 
 const isOrphanInstance = (primaryComponent: ComponentNode | ComponentSetNode): boolean => {
-  return primaryComponent.parent === null || primaryComponent.remote;
+  return primaryComponent.parent === null && primaryComponent.remote === false;
 };
 
 const isComponentRoot = (node: InstanceNode): boolean => {
