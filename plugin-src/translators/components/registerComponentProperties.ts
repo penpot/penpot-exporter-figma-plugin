9cbf4a7ad5d416832c99a2b44d4d8723b0505dd5
Jordi Sala Morales <jordism91@gmail.com>
2026-04-01 12:49:29 +0200

    Add SLOT to ComponentProperty type

--- plugin-src/translators/components/registerComponentProperties.ts
+++ plugin-src/translators/components/registerComponentProperties.ts
@@ -1,12 +1,10 @@
 import { componentProperties } from '@plugin/libraries';
 
-import type { ComponentProperty } from '@ui/types';
-
 export const registerComponentProperties = (node: ComponentSetNode | ComponentNode): void => {
   try {
     Object.entries(node.componentPropertyDefinitions).forEach(([key, value]) => {
       if (value.type === 'TEXT' || value.type === 'BOOLEAN') {
-        componentProperties.set(key, value as ComponentProperty);
+        componentProperties.set(key, value);
       }
     });
   } catch (error) {
